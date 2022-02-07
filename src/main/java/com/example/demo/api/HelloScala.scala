d0e40b1eb89a3d78b723e2f79a8075f67597fe8b
yzxingege <yzxingege@sina.com>
2022-02-07 18:45:08 +0800

    xxx

---
+++ src/main/java/com/example/demo/api/HelloScala.scala
@@ -0,0 +1,12 @@
+package com.example.demo.api
+object HelloScala {
+
+  /**
+   * hello world from scala
+   * @param args
+   */
+  def main(args: Array[String]): Unit = {
+    println("hello world from scala")
+  }
+
+}
